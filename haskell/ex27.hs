ffa366980b3ae2aac43128c0227168252121a1ac
scamgi <scampini.giacomo@gmail.com>
2025-08-01 11:46:06 +0200

    ex27.hs done (but it was very diffucult. To check it again

---
+++ haskell/ex27.hs
@@ -0,0 +1,24 @@
+{-
+Exercise 27: A Foldable Deque (from 2022.07.06) Consider a Deque a implemented with two lists: data Deque a = Deque [a] [a] where the first list is the front and the second is the reversed back. Make this an instance of Foldable. A helper function toList :: Deque a -> [a] would make this much easier.
+-}
+
+data Deque a = Deque [a] [a]
+
+{- WRONG
+toList :: Deque a -> [a]
+toList lst = concatMap toList lst
+
+instance Foldable Deque where
+  foldr f z v = f (toList v) z
+-}
+
+data Deque a = Deque [a] [a] deriving Show
+
+-- The correct toList helper function
+toList :: Deque a -> [a]
+toList (Deque front back) = front ++ reverse back
+
+-- The correct Foldable instance
+instance Foldable Deque where
+  -- To fold a Deque, just convert it to a list and use the list's foldr.
+  foldr f z deq = foldr f z (toList deq)
