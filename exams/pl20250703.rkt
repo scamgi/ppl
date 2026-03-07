426b69fdfecd9552e8ebc633a4af309cdb12d0e4
Giacomo <scampini.giacomo@gmail.com>
2026-03-07 12:19:04 +0100

    remove exams folder

--- exams/pl20250703.rkt
+++
@@ -1,9 +0,0 @@
-(define (contains-length? lst)
-    (define (contains-length-helper l remaining)
-        (cond
-            ; base case: remaining is empty so I return true
-            [(empty? remaining) #t]
-            
-            ; case 1: 
-            ))
-    (contains-length-helper (length lst) lst))
